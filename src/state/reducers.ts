f1d86b6f7450cfda3ac8b1c442fc8dbecb8bc67c
M.D. van Es <es@mdworld.nl>
2017-10-17 15:16:15 +0200

    typescript attempt and blog post

---
+++ src/state/reducers.ts
@@ -0,0 +1,25 @@
+import CONSTANTS from './constants';
+
+const REDUCERS:any = {};
+
+const initialState = {
+    selectedTerm: null
+};
+
+REDUCERS.term = function (state, action) {
+    if (typeof state === 'undefined') {
+        state = initialState;
+    }
+
+    switch (action.type) {
+        // TODO refactor condition like HomeRemote
+        case CONSTANTS.TERM_SELECTED:
+            return Object.assign({}, state, {
+                selectedTerm: action.selectedTerm
+            });
+        default:
+            return state;
+    }
+};
+
+export default REDUCERS;
