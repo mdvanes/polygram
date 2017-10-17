2ff1504cedbffa55e6baf3b9d32a4f0cf952dfa8
M.D. van Es <es@mdworld.nl>
2017-10-17 14:10:49 +0200

    Redux attempt

--- src/PolygramApp.ts
+++ src/PolygramApp.ts
@@ -1,6 +1,7 @@
 // Importing from node_modules
 //import '../bower_components/polymer/polymer-element.html'
 import { format } from 'date-fns';
+//const ReduxMixin = require('exports-loader?PolymerRedux!./ReduxMixin');
 
 //import Polymer from '../bower_components/polymer/polymer-element.html';
 //const Polymer = require('../bower_components/polymer/polymer-element.html');
@@ -27,9 +28,15 @@ const label: string = 'Current Date: ';
 //     }
 // }
 
+//const Polygram = require('imports-loader?window=>{}!exports-loader?window.Polygram!polymer-webpack-loader!./redux-mixin.html');
+import {ReduxMixin, reduxStore} from './ReduxMixin';
+
 function create(Polymer) {
-    return class PolygramApp extends Polymer.Element {
+    return class PolygramApp extends ReduxMixin(Polymer.Element) {
+        // TODO needs the Redux properties from /polygram-element.html
+
         static get is() { return 'polygram-app'; }
+
         static get properties() {
             return {
                 today: {
@@ -37,9 +44,29 @@ function create(Polymer) {
                     value: function() {
                         return label + format(new Date(), 'YYYY-MM-DD');
                     }
-                }
+                },
+                state: {
+                    type: Object,
+                    value: reduxStore.getState()
+                },
+                selectedTerm: String
             }
         }
+
+        ready() {
+            super.ready();
+            // Either do this to propagate state, or require polygram-details to extend ReduxMixin(Polymer.Element)
+            // and set its property "term" to:
+            //  type: String,
+            //  observer: '_termChanged',
+            //  statePath: 'term.selectedTerm'
+            // This would also require _standaloneTermChanged to be able to set the term without Redux, e.g. on the demo page.
+            this.addEventListener('state-changed', event => {
+                const state = event.detail;
+                this.selectedTerm = state.term.selectedTerm;
+            });
+        }
+
     }
 }
 
