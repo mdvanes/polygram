5b4436830eab9f419cf204c05d9f3f894b567377
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-10-17 20:41:30 +0200

    Require from bower_components

--- src/state/ReduxMixin.ts
+++ src/state/ReduxMixin.ts
@@ -2,8 +2,19 @@ import {combineReducers, compose, createStore} from 'redux';
 declare let __REDUX_DEVTOOLS_EXTENSION__: any; // May be added by the Chrome plugin
 // const Polygram = require('imports-loader?window=>{}!exports-loader?window.Polygram!polymer-webpack-loader!./redux-mixin.html');
 
+// const PolymerRedux = require('exports-loader?PolymerRedux!polymer-webpack-loader!debug-loader?id=raw!../../bower_components/polymer-redux/dist/polymer-redux.html');
 // tslint:disable-next-line
-const PolymerRedux = require('exports-loader?PolymerRedux!./PolymerRedux'); // TODO attempt to use polymer-loader to get from bower
+const PolymerRedux = require('exports-loader?PolymerRedux!../../bower_components/polymer-redux/dist/polymer-redux.html');
+// const PolymerRedux = require('exports-loader?PolymerRedux!debug-loader?id=raw!../../bower_components/polymer-redux/dist/polymer-redux.html');
+// FALSE --- Seems to ignore the lines with the <script> tags on it, which includes the `var PolymerRedux =` part...
+// Actually seems to add lines to the beginning of the raw import ?? Possibly: https://github.com/babel/babel/issues/4709
+// const PolymerRedux1 = require('exports-loader?PolymerRedux!debug-loader?id=raw2!./PolymerRedux');
+// TODO remove ./PolymerRedux
+
+// const PolymerRedux = require('exports-loader?PolymerRedux!polymer-webpack-loader!debug-loader!../../bower_components/polymer-redux/polymer-redux.html');
+// tslint:disable-next-line
+// const PolymerRedux = require('exports-loader?PolymerRedux!./PolymerRedux'); // TODO attempt to use polymer-loader to get from bower
+
 import REDUCERS from './reducers';
 
 // ReduxThunk (optional) for services and Redux Devtools for debugging with Chrome extension
