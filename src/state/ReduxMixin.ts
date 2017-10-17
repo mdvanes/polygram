c13f07db0e156a9520a400afdb15945f88306580
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-10-17 15:18:09 +0200

    typescript attempt and blog post

--- src/state/ReduxMixin.ts
+++ src/state/ReduxMixin.ts
@@ -1,7 +1,7 @@
 import {createStore, combineReducers, compose} from 'redux';
 declare let __REDUX_DEVTOOLS_EXTENSION__: any; // May be added by the Chrome plugin
-const PolymerRedux = require('exports-loader?PolymerRedux!./PolymerRedux'); // TODO attempt to use polymer-loader to get from bower
-import REDUCERS from './state/reducers';
+const PolymerRedux = require('exports-loader?PolymerRedux!../PolymerRedux'); // TODO attempt to use polymer-loader to get from bower
+import REDUCERS from './reducers';
 
 // ReduxThunk (optional) for services and Redux Devtools for debugging with Chrome extension
 export const reduxStore = createStore(
