3b161dc59efa2bdf18c1c0e3ab5c952b9d72b03f
M.D. van Es <es@mdworld.nl>
2017-10-17 19:32:24 +0200

    Cleaning up

--- src/PolygramApp.ts
+++ src/PolygramApp.ts
@@ -1,39 +1,19 @@
 // Importing from node_modules
-//import '../bower_components/polymer/polymer-element.html'
 import { format } from 'date-fns';
-//const ReduxMixin = require('exports-loader?PolymerRedux!./ReduxMixin');
+import {ReduxMixin, reduxStore} from './state/ReduxMixin';
 
-//import Polymer from '../bower_components/polymer/polymer-element.html';
-//const Polymer = require('../bower_components/polymer/polymer-element.html');
+// import '../bower_components/polymer/polymer-element.html'
+// import Polymer from '../bower_components/polymer/polymer-element.html';
+// const Polymer = require('../bower_components/polymer/polymer-element.html');
 // class StubElem {}
 // const Polymer = {
 //     Element: StubElem
 // }; // TODO unstub / HoF/factory ... alternatively try https://github.com/aruntk/wc-loader ?
-//
-// console.log('PolygramApp.ts', Polymer.Element);
 
 const label: string = 'Current Date: ';
 
-// class PolygramApp /*extends Polymer.Element*/ {
-//     static get is() { return 'polygram-app'; }
-//     static get properties() {
-//         return {
-//             today: {
-//                 type: String,
-//                 value: function() {
-//                     return label + format(new Date(), 'YYYY-MM-DD');
-//                 }
-//             }
-//         }
-//     }
-// }
-
-//const Polygram = require('imports-loader?window=>{}!exports-loader?window.Polygram!polymer-webpack-loader!./redux-mixin.html');
-import {ReduxMixin, reduxStore} from './state/ReduxMixin';
-
 function create(Polymer) {
     return class PolygramApp extends ReduxMixin(Polymer.Element) {
-        // TODO needs the Redux properties from /polygram-element.html
 
         static get is() { return 'polygram-app'; }
 
@@ -71,4 +51,3 @@ function create(Polymer) {
 }
 
 export default { create }
-// window.customElements.define(PolygramApp.is, PolygramApp);
