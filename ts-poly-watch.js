4a149739aaa85524693c2e9bea8129593c17258a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2017-10-30 20:10:38 +0100

    TS to JS without webpack

--- ts-poly-watch.js
+++ ts-poly-watch.js
@@ -1,59 +1,36 @@
 const watch = require('watch');
 const path = require('path');
 const chalk = require('chalk');
-//const { execFile } = require('child_process');
 const tsc = require('node-typescript-compiler');
 
-// return tsc.compile({
-//     'help': true
-// })
+const options = {
+    'target': 'ES6',
+    'sourceMap': true
+};
+
+function compileTs(path) {
+    // tsc --target ES6 --sourceMap [changedFile]
+    tsc.compile(options, path)
+        .then(_ => console.log(chalk.green.bold(`👍${path}`)))
+        .catch(err => console.log(chalk.red(err.stdout)));
+}
 
 watch.createMonitor(__dirname, { interval: 1 }, function (monitor) {
     console.log(chalk.gray.bgGreen.bold('TS-POLY-WATCH started'));
+
+    monitor.on('changed', function (filePath, curr, prev) {
+        const ext = path.extname(filePath);
+        if(ext === '.ts') {
+            console.log(`↻ ${filePath} changed`);
+            // TODO Alternatively, see https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
+            compileTs(filePath)
+        }
+    });
+
     // monitor.on("created", function (f, stat) {
     //     console.log(f + " created");
     // });
 
-    //const x = '/home/martin/ZNoBackup/polygram/polygram-marvel-details.ts';
-    // execFile('./node_modules/.bin/tsc', ['--target ES6', '--sourceMap', x], (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.log(chalk.red(stderr, error));
-    //         throw error;
-    //     }
-    //     console.log(stdout);
-    // });
-
-    // tsc.compile(
-    //     {
-    //         'target': 'ES6',
-    //         'sourceMap': true
-    //     },
-    //     x
-    // );
-
-
-    monitor.on('changed', function (f, curr, prev) {
-        const ext = path.extname(f);
-        if(ext === '.ts') {
-            console.log(f + ' changed');
-            // TODO https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
-            // tsc --target ES6 --sourceMap [changedFile]
-            // execFile('./node_modules/.bin/tsc', ['--target ES6', '--sourceMap', f], (error, stdout, stderr) => {
-            //     if (error) {
-            //         console.log(chalk.red(stderr, error));
-            //         throw error;
-            //     }
-            //     console.log(stdout);
-            // });
-            tsc.compile(
-                {
-                    'target': 'ES6',
-                    'sourceMap': true
-                },
-                f
-            );
-        }
-    });
     // monitor.on("removed", function (f, stat) {
     //     console.log(f + " removed");
     // });
